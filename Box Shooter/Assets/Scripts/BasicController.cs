be9407f2a0a1e8cc4e66dd68f98c3cc944a09eae
hossamhassancs <hossamhassan.csd@gmail.com>
2019-06-19 22:28:10 +0200

    first commit

---
+++ Box Shooter/Assets/Scripts/BasicController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicController : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		// Outputting the current Horizontal access value
+		Debug.Log("Horizontal Input = "+Input.GetAxis("Horizontal"));
+	}
+}
---
+++ Box Shooter/Assets/Scripts/BasicTargetMover.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicTargetMover : MonoBehaviour {
+
+	public float spainSpeed = 180.0f; 
+	public float motionMagnitude = 0.1f; 
+
+	public bool doSpain = true; 
+	public bool doMotion = false; 
+
+	// Update is called once per frame
+	void Update () {
+		if (doSpain) {
+			// Rotate around the up axis of the gameobject
+			gameObject.transform.Rotate (Vector3.up * spainSpeed * Time.deltaTime);
+		}
+
+		if (doMotion) {
+			// Move up and down over time
+			gameObject.transform.Translate (Vector3.up * Mathf.Cos(Time.timeSinceLevelLoad) * motionMagnitude); 
+		}
+	}
+}
